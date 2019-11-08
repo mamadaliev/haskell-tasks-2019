c3ab69473608fd0aed4503f8974a0d3e7b58e3ab
egnaf <mamadaliev@yahoo.com>
2019-11-08 23:16:17 +0300

    Add input/output of coordinates for circle and line

--- 02-find-intersection/main.hs
+++ 02-find-intersection/main.hs
@@ -1,3 +1,28 @@
-main = do
-    print "Hello, World!"
+import System.IO
 
+-- Determine whether the circle intersects with the line
+main = do
+    hSetEncoding stdin utf8
+
+    print ("Write coordinates of a circle:")
+    print ("x:")
+    circle_x <- getLine
+    print ("y:")
+    circle_y <- getLine
+    print ("R:")
+    circle_r <- getLine
+
+    print ("Write coordinates of start line:")
+    print ("x:")
+    line_x_start <- getLine
+    print ("y:")
+    line_y_start <- getLine
+
+    print ("Write coordinates of end line:")
+    print ("x:")
+    line_x_end <- getLine
+    print ("y:")
+    line_y_end <- getLine
+
+    print ("circle (x: " ++ circle_x ++ ", y: " ++ circle_y ++ ", R: " ++ circle_r ++ ")")
+    print ("Line A(x: " ++ line_x_start ++ ", y: " ++ line_y_start ++ ")" ++ ", B(x: " ++ line_x_end ++ ", y: " ++ line_y_end ++ ")")
